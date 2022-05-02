69411decfad82ebbcc61719f5c60515ad26b1124
Svetlana Batura <melkaya2025sveta@gmail.com>
2022-05-03 00:52:30 +0300

    feat: add a solution for Carbon dating

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,10 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function getCommonCharacterCount(strOne, strSecond) {
+  let arrayStrOne = strOne.split(''); // преобразовать строка в массив;
+  let arrayStrSecond = strSecond.split('');
+  let result = 0;
+  let count;
+
+  for (let i = 0; i < arrayStrOne.length; i++){
+    let index = arrayStrSecond.indexOf(arrayStrOne[i]);
+
+      if (index !== -1) {
+        result++;
+        arrayStrSecond.splice(index, 1);
+      };
+
+  };
+    count = result;
+    return count;
+};
 
 module.exports = {
   getCommonCharacterCount
